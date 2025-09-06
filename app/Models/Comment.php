5ae5ed224553e7f895403ff693d82b7465a38061
cpanelmt-source <cpanelmt@gmail.com>
2025-09-06 11:38:51 +0000

    Add mass assignable attributes to Comment and PostInteraction models

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'post_slug',
+        'author_name',
+        'author_email',
+        'content',
+        'is_approved',
+    ];
 }
--- app/Models/PostInteraction.php
+++ app/Models/PostInteraction.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class PostInteraction extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'post_slug',
+        'likes_count',
+    ];
 }
